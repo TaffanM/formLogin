9a206efb376fb7965b1038dce63297f7168659b5
Exorneur <taffanm@gmail.com>
2022-11-02 23:12:24 +0700

    add css on table

--- tampil.php
+++ tampil.php
@@ -2,16 +2,17 @@
 <head>
 	<title>tampil data</title>
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<link href="./css/tampil.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
- 
-    <form method="POST" action="tambah.php">
-    <button type="input" class="btn btn-outline-primary">Tambah</button>
-
-    </form>
-	<table class="table table-striped">
-		<tr>
+	<div class="container">
+		<form method="POST" action="tambah.php">
+			<button type="input" class="button">Tambah</button>
+		</form>
+	</div>
+	<table>
+		<tr id="header">
 			<th>No</th>
 			<th>Username</th>
 			<th>Password</th>
@@ -30,8 +31,8 @@
 				<td><?php echo $d['password']; ?></td>
 				<td><?php echo $d['level']; ?></td>
 				<td>
-					<a role ="button" class="btn btn-info" href="edit.php?id=<?php echo $d['iduser']; ?>">UBAH</a> </button>
-					<a role ="button" class="btn btn-danger" href="hapus.php?id=<?php echo $d['iduser']; ?>">HAPUS</a>
+					<a role ="button" class="btn btn-info" href="edit.php?id=<?php echo $d['ID_User']; ?>">UBAH</a> </button>
+					<a role ="button" class="btn btn-danger" href="hapus.php?id=<?php echo $d['ID_User']; ?>">HAPUS</a>
 				</td>
 			</tr>
 			<?php 
