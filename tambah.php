9e8ab2f9c3a1111b01045c88c5a6957505b40b1e
Exorneur <taffanm@gmail.com>
2022-11-03 19:55:44 +0700

    add css on tambah.php, fixing some css on formlogin.php and tampil.php, and adding background image

--- tambah.php
+++ tambah.php
@@ -1,8 +1,55 @@
+
+
 <!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="./css/tambah.css" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<title>Tambah</title>
+</head>
+<body>
+<div class="container">
+	<div class="header">
+		<h2>Tambah Data Pengguna</h2>
+	</div>
+	<form method="post" action="inputuser.php" class="form">
+		<div class="form-control">
+			<label for="username">Username</label>
+			<input type="text" placeholder="Masukkan username" name="username" />
+			<i class="fas fa-check-circle"></i>
+			<i class="fas fa-exclamation-circle"></i>
+			<small>Error message</small>
+		</div>
+		<div class="form-control">
+			<label for="username">Password</label>
+			<input type="text" placeholder="Masukkan password" name="password" />
+			<i class="fas fa-check-circle"></i>
+			<i class="fas fa-exclamation-circle"></i>
+			<small>Error message</small>
+		</div>
+		<div class="form-control">
+			<label for="username">Level</label>
+			<input type="text" placeholder="Masukkan level" name="level"/>
+			<i class="fas fa-check-circle"></i>
+			<i class="fas fa-exclamation-circle"></i>
+			<small>Error message</small>
+		</div>
+		<input type="submit" value="SIMPAN" class="button">
+	</form>
+</div>
+</body>
+</html>
+
+<!-- <!DOCTYPE html>
 <html>
 <head>
 	<title>tambah data</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="./css/tambah.css" rel="stylesheet">
 </head>
 <body>
 	<br/>
@@ -31,4 +78,4 @@
 		</table>
 	</form>
 </body>
-</html>
+</html> -->
--- tampil.php
+++ tampil.php
@@ -7,37 +7,38 @@
 </head>
 <body>
 	<div class="container">
-		<form method="POST" action="tambah.php">
-			<button type="input" class="button">Tambah</button>
-		</form>
-	</div>
-	<table>
-		<tr id="header">
-			<th>No</th>
-			<th>Username</th>
-			<th>Password</th>
-			<th>Level</th>
-			<th>Aksi</th>
-		</tr>
-		<?php 
-		include 'koneksi.php';
-		$no = 1;
-		$data = mysqli_query($koneksi,"select * from user");
-		while($d = mysqli_fetch_array($data)){
-			?>
-			<tr>
-				<td><?php echo $no++; ?></td>
-				<td><?php echo $d['username']; ?></td>
-				<td><?php echo $d['password']; ?></td>
-				<td><?php echo $d['level']; ?></td>
-				<td>
-					<a role ="button" class="btn btn-info" href="edit.php?id=<?php echo $d['ID_User']; ?>">UBAH</a> </button>
-					<a role ="button" class="btn btn-danger" href="hapus.php?id=<?php echo $d['ID_User']; ?>">HAPUS</a>
-				</td>
+			<form method="POST" action="tambah.php">
+				<button type="input" class="button">Tambah</button>
+			</form>
+		
+		<table>
+			<tr id="header">
+				<th>No</th>
+				<th>Username</th>
+				<th>Password</th>
+				<th>Level</th>
+				<th>Aksi</th>
 			</tr>
 			<?php 
-		}
-		?>
-	</table>
+			include 'koneksi.php';
+			$no = 1;
+			$data = mysqli_query($koneksi,"select * from user");
+			while($d = mysqli_fetch_array($data)){
+				?>
+				<tr>
+					<td><?php echo $no++; ?></td>
+					<td><?php echo $d['username']; ?></td>
+					<td><?php echo $d['password']; ?></td>
+					<td><?php echo $d['level']; ?></td>
+					<td>
+						<a role ="button" class="btn btn-info" href="edit.php?id=<?php echo $d['ID_User']; ?>">UBAH</a> </button>
+						<a role ="button" class="btn btn-danger" href="hapus.php?id=<?php echo $d['ID_User']; ?>">HAPUS</a>
+					</td>
+				</tr>
+				<?php 
+			}
+			?>
+		</table>
+	</div>
 </body>
 </html>
